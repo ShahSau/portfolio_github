7088e2fdea2669ed90c57a7b053d68ecf09fd39f
ShahSau <shahriarksaurov@gmail.com>
2021-11-09 14:57:02 +0200

    update logo locations

--- src/data/skills.js
+++ src/data/skills.js
@@ -4,6 +4,36 @@ import c from '../img/skills/c.png'
 import Cs from '../img/skills/CSharp.png'
 import R from '../img/skills/R2.png'
 
+
+//froentend
+import html from '../img/skills/html.png'
+import css from '../img/skills/css.png'
+import react from '../img/skills/react.png'
+import TS from '../img/skills/TS.png'
+import vue from '../img/skills/vue.png'
+import redux from '../img/skills/redux.svg'
+import boot from '../img/skills/boot.png'
+import material from '../img/skills/material.png'
+import tail from '../img/skills/tail.svg'
+
+//backend
+import node from '../img/skills/node.png'
+import express from '../img/skills/express.png'
+import flask from '../img/skills/flask.png'
+import django from '../img/skills/django.png'
+import djREST from '../img/skills/djREST.webp'
+import graph from '../img/skills/graph.png'
+import rest from '../img/skills/rest.png'
+
+///testing
+import jest from '../img/skills/jest.png'
+import cypress from '../img/skills/cypress.png'
+import mocha from '../img/skills/mocha.png'
+import aws from '../img/skills/aws.png'
+import heroku from '../img/skills/heroku.png'
+import netlify from '../img/skills/netlify.png'
+
+
 import docker from '../img/skills/docker.png'
 const skills = [
   {
@@ -21,48 +51,57 @@ const skills = [
   {
     id: 1,
     title: 'Froentend Technologies',
-    image: ['https://i.ibb.co/4YCXwf6/file-type-html-icon-130541.png',
-      'https://i.ibb.co/8dyJw6n/file-type-css-icon-130661.png',
-      'https://i.ibb.co/02Pnk3p/react-original-wordmark-logo-icon-146375.png',
-      'https://i.ibb.co/6DnrchH/file-type-typescript-official-icon-130107.png',
-      'https://i.ibb.co/XyhFHHR/file-type-vue-icon-130078.png',
-      'https://i.ibb.co/fQbMmqY/bootstrap-plain-wordmark-logo-icon-146620.png',
-      'https://seeklogo.com/images/M/material-ui-logo-5BDCB9BA8F-seeklogo.com.png',
+    image: [
+    html,
+    css,
+    react,
+    redux,
+    TS,
+    vue,
+    boot,
+    material,
+    tail
     ],
   },
   {
     id: 2,
     title: 'Backend Technologies',
-    image: ['https://i.ibb.co/7bkqdYs/nodejs-plain-wordmark-logo-icon-146410.png',
-
-      'https://i.ibb.co/yqRqNS8/kisspng-node-js-express-js-javascript-solution-stack-web-a-5b22b9d57436d2-563861541529002453476-remo.png',
-      'https://i.ibb.co/TmsK68x/flask.png',
-      'https://i.ibb.co/68fCgL2/file-type-graphql-icon-130564.png',
-      'https://i.ibb.co/c2987nZ/django-plain-logo-icon-146558.png',
-      'https://i.ibb.co/8rNQNtb/rest.png',
+    image: [
+      node,
+      express,
+      flask,
+      django,
+      djREST,
+      graph,
+      rest,
+    
     ],
   },
   {
     id: 3,
-    title: 'Data Analysis',
+    title: 'Testing & Cloud',
     image: [
-
-      'https://user-images.githubusercontent.com/1217238/65354639-dd928f80-dba4-11e9-833b-bc3e8c6a737d.png',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Pandas_logo.svg/450px-Pandas_logo.svg.png',
-      'https://static.bokeh.org/logos/logotype.svg',
-      'https://images.prismic.io/plotly-marketing-website/bd1f702a-b623-48ab-a459-3ee92a7499b4_logo-plotly.svg',
-      'https://seaborn.pydata.org/_static/logo-wide-lightbg.svg',
-      'https://tidyverse.tidyverse.org/articles/tidyverse-logo.png',
+      jest,
+      cypress,
+      mocha,
+      heroku,
+      aws,
+      netlify
 
     ],
   },
   {
     id: 4,
-    title: 'Machine learning',
+    title: 'Machine learning & Data Science',
     image: [
       'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/260px-Scikit_learn_logo_big.svg.png',
       'https://opencv.org/wp-content/uploads/2020/07/cropped-OpenCV_logo_white_600x.png',
-      'https://spark.apache.org/images/spark-logo-trademark.png',
+      'https://spark.apache.org/images/spark-logo-trademark.png', 
+      'https://user-images.githubusercontent.com/1217238/65354639-dd928f80-dba4-11e9-833b-bc3e8c6a737d.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Pandas_logo.svg/450px-Pandas_logo.svg.png',
+      'https://static.bokeh.org/logos/logotype.svg',
+      'https://images.prismic.io/plotly-marketing-website/bd1f702a-b623-48ab-a459-3ee92a7499b4_logo-plotly.svg',
+      'https://tidyverse.tidyverse.org/articles/tidyverse-logo.png',
     ],
   },
   {
@@ -78,9 +117,10 @@ const skills = [
     id: 6,
     title: 'Others',
     image: [
-
+////figma
+///react native
       'https://i.ibb.co/KWL8Y3V/Tableau-Logo.png',
-      'https://i.ibb.co/KxLqPrz/heroku-logo-1.png',
+      
       'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/150px-Tux.svg.png',
       docker,
 
